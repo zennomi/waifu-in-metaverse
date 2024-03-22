638bf2217383d24728042bf40682beb00a2d28ce
zen-lenovo <zennomi.kun@gmail.com>
2024-03-22 22:06:01 +0700

    add r3f

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Suspense } from 'react';
+import { KeyboardControls, PointerLockControls, Sky } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { VRButton, ARButton, XR, Controllers, Hands, TeleportationPlane } from '@react-three/xr'
 import './App.css';
+import { Physics } from '@react-three/rapier';
+import { Ground } from './Ground';
+import { Player } from './Player';
+import { Gallery } from './Gallery';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <KeyboardControls
+        map={[
+          { name: "forward", keys: ["ArrowUp", "w", "W"] },
+          { name: "backward", keys: ["ArrowDown", "s", "S"] },
+          { name: "left", keys: ["ArrowLeft", "a", "A"] },
+          { name: "right", keys: ["ArrowRight", "d", "D"] },
+          { name: "jump", keys: ["Space"] },
+        ]}>
+        <Canvas shadows camera={{ fov: 45 }}>
+          <Sky sunPosition={[100, 20, 100]} />
+          <ambientLight intensity={5} />
+          <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+
+          <Suspense fallback={null}>
+            <Physics>
+              <Gallery />
+              <Ground />
+              <Player />
+            </Physics>
+          </Suspense>
+          <PointerLockControls />
+        </Canvas>
+      </KeyboardControls> */}
+      <VRButton />
+      <Canvas>
+        <Sky sunPosition={[100, 20, 100]} />
+        <ambientLight intensity={5} />
+        <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+        <XR>
+          <Physics>
+            <Gallery />
+            <Ground />
+          </Physics>
+          <Controllers />
+          <Hands />
+          <TeleportationPlane
+            /** Whether to allow teleportation from left controller. Default is `false` */
+            leftHand={true}
+            /** Whether to allow teleportation from right controller. Default is `false` */
+            rightHand={true}
+            /** The maximum distance from the camera to the teleportation point. Default is `10` */
+            maxDistance={10}
+            /** The radial size of the teleportation marker. Default is `0.25` */
+            size={0.25}
+          />
+        </XR>
+      </Canvas>
     </div>
   );
 }
